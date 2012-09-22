1f516b752d2292699247d8969c83f6a4b23a3493
s-soodmand <somaieh.soodmand@gmail.com>
2012-09-22 15:53:20 +0330

    add file for product-page

---
+++ somaye/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="1styles.css" />
+	<link rel="stylesheet" type="text/css" href="style.less" />
+
+	<script src="script/less.js" type="text/javascript"></script>
+	<!--[if IE]>
+		<script type="text/javascript" src="script/html5.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<div class="container">
+		<!-- start Header -->
+		<header class="cover">
+			<div class="logo left w5">LOGO</div>
+			<div class="menu-box ml5 w19">
+				<div class="search w4 last right"></div>
+				<nav class="menu"></nav>
+			</div>
+		</header>
+		<!-- start div product -->
+		<section class="product w24 cover mt3">
+			<!-- div product -->	
+		</section>
+		<section class="box-banner w24 cover mt4">
+			<div class="banner left w6">
+			</div>
+		</section>
+		<footer class="mt3 cover">
+			<section class="footer-box w22 mlra">
+				<div class="left-pic left"></div>
+				<div class="language w2 left">
+					<span class="language-icon left"></span>
+					<span class="next-icon right w1 last"></span>
+					<a class="mlra" href="#">?</a>	
+				</div>
+				<div class="right-pic right"></div>
+				<div class="facebook right last"></div>
+				<div class="footer-menu mlra w11"></div>
+			</section>
+			<div class="copyright w3 mlra"></div>
+		</footer>
+
+
+
+		
+	</div>
+</body>
+</html>
