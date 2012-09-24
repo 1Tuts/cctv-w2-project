c3204cae0b2506e6e967bc2b53ff534f68bbbeec
s-soodmand <somaieh.soodmand@gmail.com>
2012-09-24 15:31:32 +0330

    complate content about

--- somaye/about-index.php
+++ somaye/about-index.php
@@ -31,11 +31,31 @@
 				</ul>
 			</nav>
 		</header>
+
+		<!-- start content -->
 		<section class="about-content">
 
 			<ul class="web-nav cover mt2"></ul>
 			<div class="web-info-top"></div>
-			<div class="web-info-middle"></div>
+			<div class="web-info-middle">
+				<div class="info right last">
+					<p class="mt1">
+						Since its founding in 1976, Acer has achieved its goal of breaking barriers between people and technology. Globally, Acer ranks No. 2 for notebook PCs.1 A profitable and sustainable Channel Business Model is instrumental to the company's continuing growth, while its multi-brand approach integrates Acer, Gateway, Packard Bell, and eMachines brands in worldwide markets.
+					</p>
+					<div class="img mt2">
+						<img src="images/about/about_us_01.jpg">
+						<img src="images/about/about_us_02.jpg">
+					</div>
+					<p class="mt1">
+						Acer designs environmentally friendly products and has established a green supply chain through collaboration with suppliers. Acer is proud to be a Worldwide Partner of the Olympic Movement, including supporting the Vancouver 2010 Olympic Winter Games and London 2012 Olympic Games.
+					</p>
+					<p class="mt1">
+						The Acer Group employs 8,000 people across the globe. Revenues for 2011 reached US$15.7 billion.<br />
+						Visit <a href="#"> www.acer-group.com </a>for more information.
+					</p>
+				</div>
+				<ul class="left-menu"></ul>
+			</div>
 			<div class="web-info-bottom"></div>
 
 		</section>
