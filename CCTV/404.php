413bfa19c7a7879d97395a71e383d379ea5c31f5
zohreh-z <zahedi.zohreh@gmail.com>
2012-09-25 19:57:08 +0330

    add page not found

---
+++ CCTV/404.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>ERROR 404</title>
+    <link type="text/css" rel="stylesheet" href="1styles.css" />
+	<link rel="stylesheet/less" type="text/css" href="style.less" />
+    <link type="image/x-icon" rel="shortcut icon" href="favicon.ico" />
+	<script src="scripts/less.js" type="text/javascript"></script>
+	<!--[if IE]>
+		<script type="text/javascript" src="scripts/html5.js"></script>
+	<![endif]-->
+</head>
+
+<body class="not-found">
+	<div class=" logo"></div>	
+	<header class="mh15 cover"></header>
+
+	<section class="container cover">
+    	<div class=" message mh40">
+        	<div class="txt f9"><p>متاسفانه صفحه مورد نظر شما وجود ندارد برای رفتن به صفحه اصلی <a href="./index.php">اینجا </a>را کلیلک کنید</p></div>
+        </div>
+        <div class="copy f6">Copyright© ????? Inc. All Rights Reserved</div>
+    </section>
+</body>
+</html>
