50b8be7824d9fe368dd41d12d24c6da507bf6134
s-soodmand <somaieh.soodmand@gmail.com>
2012-09-28 11:27:54 +0330

    seprate main menu

---
+++ somaye/inc/main-menu.php
@@ -0,0 +1,22 @@
+
+			<div class="main">
+				<nav>
+					<img src="images/product/menu_background.png" />
+					<ul>
+						<li><a href="#">محصولات</a>
+							<ul>
+								<li><a href="#">محصولات</a></li>
+								<li><a href="#">اطلاعات خرید</a></li>
+								<li><a href="#">پشتیبانی</a></li>
+							</ul>	
+						</li>
+						<li><a href="#"> پشتیبانی</a>
+							<ul>
+								<li><a href="#">محصولات</a></li>
+								<li><a href="#">اطلاعات خرید</a></li>
+							</ul>
+						</li>
+						<li><a href="#">ارتباط با ما</a></li>
+					</ul>
+				</nav>
+			</div>
