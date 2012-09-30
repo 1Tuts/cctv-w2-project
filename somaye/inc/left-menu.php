7801b23b14020169ae42f04a049c2bebb4f94d75
s-soodmand <somaieh.soodmand@gmail.com>
2012-09-30 12:05:59 +0330

    seprate left-menu

---
+++ somaye/inc/left-menu.php
@@ -0,0 +1,9 @@
+<nav class="left-menu right">
+	<ul>
+		<li><a href="about-index.php">درباره ما</a></li>
+		<li><a href="#">تیم اجرایی</a></li>
+		<li><a href="#">milestones</a></li>
+		<li><a href="#">تماس با ما</a></li>
+		<li><a href="#">شرکت اجتماعی</a></li>
+	</ul>
+</nav>
