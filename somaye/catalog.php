d37eeea48837068b2bdd266fff71e71f225066c8
s-soodmand <somaieh.soodmand@gmail.com>
2012-10-01 14:34:18 +0330

    Add Catalog Page

---
+++ somaye/catalog.php
@@ -0,0 +1,36 @@
+		<?php
+			include('inc/header.php');
+		?>
+		<!-- start contact info -->
+		<section class="catalog cover">
+			<div class="nav cover mt2">
+				<a href="#">درباره شرکت</a>
+				<span></span>
+				<a class="next-link" href="#">کاتالوگ</a>
+			</div>
+			<div class="catalog-info cover">
+				<div class="left-txt">
+					<p>
+						مرور فروشگاه با کلیک بر روی گروه محصول.
+						<br />
+						<br />
+						<br />
+						ذخیره به عنوان مشاهده در قالب پی دی اف گروه محصول یا سری محصول را با کلیک بر روی دکمه خاکستری.
+					</p>
+				</div>
+				<div class="info left">
+					<ul>
+							<li><a href="#"><span class="download">صادرات این گروه</span><span class="list">نت بوک</span></a></li>
+							<li><a href="#"><span class="download">صادرات این گروه</span><span class="list">دسک تاب</span></a></li>
+							<li><a href="#"><span class="download">صادرات این گروه</span><span class="list">مانیتور</span></a></li>
+							<li><a href="#"><span class="download">صادرات این گروه</span><span class="list">پروژکتور</span></a></li>
+							<li><a href="#"><span class="download">صادرات این گروه</span><span class="list">تبلت</span></a></li>
+							<li><a href="#"><span class="download">صادرات این گروه</span><span class="list">انتخابی</span></a></li>
+							<li><a href="#"><span class="download">صادرات این گروه</span><span class="list">Ultrabooks</span></a></li>
+						</ul>	
+				</div>
+			</div>
+		</section>
+		<?php
+			include('inc/footer.php');
+		?>
