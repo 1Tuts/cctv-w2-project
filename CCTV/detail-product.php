04f40d8d43b4bffce4293991d54c6430eb4a769b
zohreh-z <zahedi.zohreh@gmail.com>
2012-10-01 16:06:15 +0330

    change detail gallery product but not complete

--- CCTV/detail-product.php
+++ CCTV/detail-product.php
@@ -1,60 +1,13 @@
-		<?php
-			include('inc/header.php');
-		?>
-		<section class="product-camera-content txr cover">
-			<?php
-				include "inc/detail-product-menu.php";
-			?>
-			<section class="detail-pr cover">
-            	<article class="txt-detail-pr">
-                	<h3>P SERIES</h3>
-                    <p>
-                    	ایسر سری P ویترین، تصاویر شگفت انگیز برای لذت بردن چند رسانه ای عالی، گرافیک برنامه های کاربردی فشرده، و مرور اینترنت می باشد.تعادل کامل بین صفحه نمایش جذاب، عملکرد قابل اعتماد، و فن آوری های سازگار با محیط زیست آگاه می سازد این سری ایده آل برای رسانه های خانگی و برنامه های کاربردی سینمایی.
-
-                    </p>
-                </article>
-                <img src="images/product/detail-product/P_350x250.jpg" />
-            </section>
-				<div class="info left cover">
-                	<div class="share-property">
-                    	<img src="images/product/detail-product/PSeries_elegant_design.jpg"/>
-                        <h3>طراحی زیبا و کارآمد</h3>
-                        <p>
-                        	قاب براق صفحه نمایش، الگوی پایه دور، و از نقطه ایستادگی این سری ظرافت سن جدید که نوسازی هر اتاق در خانه است. به علاوه، دکمه قدرت های بزرگ در گوشه پایین سمت راست چراغ به رنگ آبی روشن زمانی که دستگاه روشن باشد، برای دسترسی آسان و شیک نگاه.
-
-                        </p>
-                    </div>
-                    <div class="share-property">
-                    	<img src="images/product/detail-product/PSeries_luxurious_details.jpg" />
-                        <h3>جزئیات لوکس</h3>
-                        <p>
-                        	با وضوح بالا، زمان پاسخ سریع، و نسبت کنتراست برتر را به این ایده آل سری برای مشاهده سنگین رسانه ها برنامه ها و فیلم های سینمایی را با دقت بیشتر و تصاویر واضح.
-
-                        </p>
-                    </div>
-                    <div class="share-property">
-                    	<img src="images/product/detail-product/PSeries_luxurious_details.jpg" />
-                        <h3>دیجیتال اتصال </h3>
-                        <p>
-اتصال به طور گسترده استفاده می شود دستگاه های دیجیتال HD از طریق HDMI ® interface1، و لذت بردن از DRM محافظت شده به لطف کامل با کیفیت سرگرمی را به پورت DVI با حفاظت از محتوای دیجیتال با پهنای باند بالا (HDCP).
-
-                        </p>
-                    </div>
-                    <div class="share-property last-pr">
-                    	<img src="images/product/detail-product/PSeries_luxurious_details.jpg" />
-                        <h3>زمین مراقبت پس انداز قدرت </h3>
-                        <p>
-جیوه تکنولوژی نور پس زمینه LED به حداقل تضمین های دادرسی استفاده ایمن و مراقبت از محیط زیست است. تکنولوژی LED نیز از انرژی کمتر استفاده می کند، ارائه تا 68٪ صرفه جویی در قدرت است.
-
-                        </p>
-                    </div>
-                    <div class="bottom-bg"></div>
-				</div>
-				
-			</div>
-			
-		</section>
-
-		<?php
-			include('inc/footer.php');
-		?>
+<?php
+	global $product;
+	include 'inc/header.php';
+	include "inc/detail-product-menu.php";
+	if ($product === 'mazaya'){	
+		include 'inc/body-detail-product.php';
+		
+	}elseif($product === 'gallery'){
+		include 'detail-product-gallery.php';
+	}
+	
+	include 'inc/footer.php';
+?>
--- CCTV/inc/detail-product-menu.php
+++ CCTV/inc/detail-product-menu.php
@@ -1,12 +1,28 @@
-<div class="nav cover mt2">
-	<a href="#">دوربین  </a>
-	<a class="next-link" href="#"> ما</a>
-</div>
-<div class="content-pr cover">
-	<nav class="content-pr-nav">
-		<ul>
-            <li class="current"><a href="#"> مزایای</a></li>
-        	<li><a href="#"> گالری عکس</a></li>
-            <li><a href="#"> مدل</a></li>
-    	</ul>
-    </nav>
+<section class="product-camera-content txr cover">
+	<div class="nav cover mt2">
+		<a href="#">دوربین  </a>
+		<a class="next-link" href="#"> ما</a>
+	</div>
+	<div class="content-pr cover">
+		<nav class="content-pr-nav">
+			<ul>
+	            <li>
+                	<a href="./detail-product.php">
+	                 مزایای	
+	                </a>
+                    <?php
+						$product = 'mazaya';
+					?>
+                 </li>
+	        	<li>
+                	<a href="./detail-product.php">
+	                     گالری عکس	
+                    </a>
+                    <?php
+						$product = 'gallery';
+					?>
+                </li>
+	            <li><a href="#"> مدل</a></li>
+	    	</ul>
+	    </nav>
+        
