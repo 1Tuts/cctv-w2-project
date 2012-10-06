ed2ec0b67ef65570320447089680dc61e1b1f2d2
s-soodmand <somaieh.soodmand@gmail.com>
2012-10-06 15:09:04 +0330

    add link page ...

--- somaye/inc/main-menu.php
+++ somaye/inc/main-menu.php
@@ -14,29 +14,28 @@ $(document).ready(function () {
 </script>
 <nav class="mainmenu">
 	<ul id="nav">
-		<li><a href="#">محصولات</a>
+		<li><a href="product-index.php">محصولات</a>
 			<ul>
 				<li class="active"><a href='#'>مصرف کننده</a></li>
-				<li><a href='#'>نوت بوک</a></li>
-				<li><a href='#'>ultrabooks</a></li>
-				<li><a href='#'>نت بوک</a></li>
-				<li><a href='#'>تبلت</a></li>
-				<li><a href='#'>دسکتاپ</a></li>
-				<li><a href='#'>مانیتور</a></li>
-				<li><a href='#'>پروژکتور</a></li>
-				<li><a href='#'>انتخاب</a></li>
-				<li class="blue"><a href='#'>clear.fi</a></li>
+				<li><a href='product-detail.php'>نوت بوک</a></li>
+				<li><a href='product-detail.php'>ultrabooks</a></li>
+				<li><a href='product-detail.php'>نت بوک</a></li>
+				<li><a href='product-detail.php'>تبلت</a></li>
+				<li><a href='product-detail.php'>دسکتاپ</a></li>
+				<li><a href='product-detail.php'>مانیتور</a></li>
+				<li><a href='product-detail.php'>پروژکتور</a></li>
+				<li><a href='product-detail.php'>انتخاب</a></li>
+				<li class="blue"><a href='product-detail.php'>clear.fi</a></li>
 			</ul>	
 		</li>
 		<li><a href="#"> پشتیبانی</a>
 			<ul>
-				<li><a href="#">مزیت ایسر</a></li>
-				<li><a href="#">درایور ها و دانلود</a></li>
+				<li><a href="buy-index.php">خرید</a></li>
+				<li><a href="catalog-index.php">دانلود</a></li>
 				<li><a href="#">آپدیت نرم افزار ها</a></li>
-				<li><a href="#"></a></li>
 			</ul>
 		</li>
-		<li><a href="#">ارتباط با ما</a></li>
+		<li><a href="contact-index.php">ارتباط با ما</a></li>
 	</ul>
 </nav>
 			
