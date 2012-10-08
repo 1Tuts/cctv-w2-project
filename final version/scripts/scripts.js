40107d2059b6fd90fc59f70534dc7518e320fed2
s-soodmand <somaieh.soodmand@gmail.com>
2012-10-08 17:09:28 +0330

    fix delay menu ...........

--- final version/scripts/scripts.js
+++ final version/scripts/scripts.js
@@ -99,12 +99,11 @@ $(function(){
 	});
 /* Detail Page
  ------------------------------------- */
-var listDetail = $('section.product-detail div.info-pro div.slide-pro div.slideshow-pro div.scroll div.detail');
-	viewDetail = $('div.img-desc div.train div.pro');
-	nameDetail = $('div.name-desc div.train div.pro')
-	descDetail = $('div.desc div.train div.pro')
-	currentDet = 0;
-
+	var listDetail = $('section.product-detail div.info-pro div.slide-pro div.slideshow-pro div.scroll div.detail');
+		viewDetail = $('div.img-desc div.train div.pro');
+		nameDetail = $('div.name-desc div.train div.pro')
+		descDetail = $('div.desc div.train div.pro')
+		currentDet = 0;
 
 	currentPic = function(n){
 		var listPic = $('section.product-detail div.info-pro div.slide-pro div.slideshow-pro div.scroll > div')
@@ -113,265 +112,264 @@ var listDetail = $('section.product-detail div.info-pro div.slide-pro div.slides
 		currentDet=n;
 	}
 
-listDetail.click(function(){
-		index = ($(this).index());
-		switch(index)
-		{
-			case 0:
-				viewDetail.css({
-					opacity:'0'
-				});
-				nameDetail.css({
-					opacity:'0'
-				});
-				descDetail.css({
-					opacity:'0'
-				});
-				$('div.img-desc div.train div#img1').css({
-					opacity:'1'
-				});
-				$('div.name-desc div.train div#name1').css({
-					opacity:'1'
-				});
-				$('div.desc div.train div#desc1').css({
-					opacity:'1'
-				});
-				currentPic( $(this).index() );
-				break;
-			case 1:
-				viewDetail.css({
-					opacity:'0'
-				});
-				nameDetail.css({
-					opacity:'0'
-				});
-				descDetail.css({
-					opacity:'0'
-				});
-				$('div.img-desc div.train div#img2').css({
-					opacity:'1'
-				});
-				$('div.name-desc div.train div#name2').css({
-					opacity:'1'
-				});
-				$('div.desc div.train div#desc2').css({
-					opacity:'1'
-				});
-				currentPic( $(this).index() );
-				break;
-			case 2:
-				viewDetail.css({
-					opacity:'0'
-				});
-				nameDetail.css({
-					opacity:'0'
-				});
-				descDetail.css({
-					opacity:'0'
-				});
-				$('div.img-desc div.train div#img3').css({
+	listDetail.click(function(){
+			index = ($(this).index());
+			switch(index)
+			{
+				case 0:
+					viewDetail.css({
+						opacity:'0'
+					});
+					nameDetail.css({
+						opacity:'0'
+					});
+					descDetail.css({
+						opacity:'0'
+					});
+					$('div.img-desc div.train div#img1').css({
 						opacity:'1'
-				});
-				$('div.name-desc div.train div#name3').css({
+					});
+					$('div.name-desc div.train div#name1').css({
 						opacity:'1'
-				});
-				$('div.desc div.train div#desc3').css({
-					opacity:'1'
-				});
-				currentPic( $(this).index() );
-				break;
-			case 3:
-				viewDetail.css({
-					opacity:'0'
-				});
-				nameDetail.css({
-					opacity:'0'
-				});
-				descDetail.css({
-					opacity:'0'
-				});
-				$('div.img-desc div.train div#img4').css({
-					opacity:'1'
-				});
-				$('div.name-desc div.train div#name4').css({
-					opacity:'1'
-				});
-				$('div.desc div.train div#desc4').css({
-					opacity:'1'
-				});
-				currentPic( $(this).index() );
-				break;
-			case 1:
-				viewDetail.css({
-					opacity:'0'
-				});
-				nameDetail.css({
-					opacity:'0'
-				});
-				descDetail.css({
-					opacity:'0'
-				});
-				$('div.img-desc div.train div#img2').css({
-					opacity:'1'
-				});
-				$('div.name-desc div.train div#name2').css({
-					opacity:'1'
-				});
-				$('div.desc div.train div#desc2').css({
-					opacity:'1'
-				});
-				currentPic( $(this).index() );
-				break;
-			case 2:
-				viewDetail.css({
-					opacity:'0'
-				});
-				nameDetail.css({
-					opacity:'0'
-				});
-				descDetail.css({
-					opacity:'0'
-				});
-				$('div.img-desc div.train div#img3').css({
+					});
+					$('div.desc div.train div#desc1').css({
 						opacity:'1'
-				});
-				$('div.name-desc div.train div#name3').css({
+					});
+					currentPic( $(this).index() );
+					break;
+				case 1:
+					viewDetail.css({
+						opacity:'0'
+					});
+					nameDetail.css({
+						opacity:'0'
+					});
+					descDetail.css({
+						opacity:'0'
+					});
+					$('div.img-desc div.train div#img2').css({
 						opacity:'1'
-				});
-				$('div.desc div.train div#desc3').css({
-					opacity:'1'
-				});
-				currentPic( $(this).index() );
-				break;
-			case 3:
-				viewDetail.css({
-					opacity:'0'
-				});
-				nameDetail.css({
-					opacity:'0'
-				});
-				descDetail.css({
-					opacity:'0'
-				});
-				$('div.img-desc div.train div#img4').css({
+					});
+					$('div.name-desc div.train div#name2').css({
 						opacity:'1'
-				});
-				$('div.name-desc div.train div#name4').css({
+					});
+					$('div.desc div.train div#desc2').css({
 						opacity:'1'
-				});
-				$('div.desc div.train div#desc4').css({
-					opacity:'1'
-				});
-				currentPic( $(this).index() );
-				break;
-			case 4:
-				viewDetail.css({
-					opacity:'0'
-				});
-				nameDetail.css({
-					opacity:'0'
-				});
-				descDetail.css({
-					opacity:'0'
-				});
-				$('div.img-desc div.train div#img5').css({
+					});
+					currentPic( $(this).index() );
+					break;
+				case 2:
+					viewDetail.css({
+						opacity:'0'
+					});
+					nameDetail.css({
+						opacity:'0'
+					});
+					descDetail.css({
+						opacity:'0'
+					});
+					$('div.img-desc div.train div#img3').css({
+							opacity:'1'
+					});
+					$('div.name-desc div.train div#name3').css({
+							opacity:'1'
+					});
+					$('div.desc div.train div#desc3').css({
 						opacity:'1'
-				});
-				$('div.name-desc div.train div#name5').css({
+					});
+					currentPic( $(this).index() );
+					break;
+				case 3:
+					viewDetail.css({
+						opacity:'0'
+					});
+					nameDetail.css({
+						opacity:'0'
+					});
+					descDetail.css({
+						opacity:'0'
+					});
+					$('div.img-desc div.train div#img4').css({
 						opacity:'1'
-				});
-				$('div.desc div.train div#desc5').css({
-					opacity:'1'
-				});
-				currentPic( $(this).index() );
-				break;
-			case 5:
-				viewDetail.css({
-					opacity:'0'
-				});
-				nameDetail.css({
-					opacity:'0'
-				});
-				descDetail.css({
-					opacity:'0'
-				});
-				$('div.img-desc div.train div#img6').css({
+					});
+					$('div.name-desc div.train div#name4').css({
 						opacity:'1'
-				});
-				$('div.name-desc div.train div#name6').css({
+					});
+					$('div.desc div.train div#desc4').css({
 						opacity:'1'
-				});
-				$('div.desc div.train div#desc6').css({
-					opacity:'1'
-				});
-				currentPic( $(this).index() );
-				break;
-			case 6:
-				viewDetail.css({
-					opacity:'0'
-				});
-				nameDetail.css({
-					opacity:'0'
-				});
-				descDetail.css({
-					opacity:'0'
-				});
-				$('div.img-desc div.train div#img7').css({
+					});
+					currentPic( $(this).index() );
+					break;
+				case 1:
+					viewDetail.css({
+						opacity:'0'
+					});
+					nameDetail.css({
+						opacity:'0'
+					});
+					descDetail.css({
+						opacity:'0'
+					});
+					$('div.img-desc div.train div#img2').css({
 						opacity:'1'
-				});
-				$('div.name-desc div.train div#name7').css({
+					});
+					$('div.name-desc div.train div#name2').css({
 						opacity:'1'
-				});
-				$('div.desc div.train div#desc7').css({
-					opacity:'1'
-				});
-				currentPic( $(this).index() );
-				break;
-			case 7:
-				viewDetail.css({
-					opacity:'0'
-				});
-				nameDetail.css({
-					opacity:'0'
-				});
-				descDetail.css({
-					opacity:'0'
-				});
-				$('div.img-desc div.train div#img8').css({
+					});
+					$('div.desc div.train div#desc2').css({
 						opacity:'1'
-				});
-				$('div.name-desc div.train div#name8').css({
+					});
+					currentPic( $(this).index() );
+					break;
+				case 2:
+					viewDetail.css({
+						opacity:'0'
+					});
+					nameDetail.css({
+						opacity:'0'
+					});
+					descDetail.css({
+						opacity:'0'
+					});
+					$('div.img-desc div.train div#img3').css({
+							opacity:'1'
+					});
+					$('div.name-desc div.train div#name3').css({
+							opacity:'1'
+					});
+					$('div.desc div.train div#desc3').css({
 						opacity:'1'
-				});
-				$('div.desc div.train div#desc8').css({
-					opacity:'1'
-				});
-				currentPic( $(this).index() );
-				break;
-			case 8:
-				viewDetail.css({
-					opacity:'0'
-				});
-				nameDetail.css({
-					opacity:'0'
-				});
-				descDetail.css({
-					opacity:'0'
-				});
-				$('div.img-desc div.train div#img9').css({
+					});
+					currentPic( $(this).index() );
+					break;
+				case 3:
+					viewDetail.css({
+						opacity:'0'
+					});
+					nameDetail.css({
+						opacity:'0'
+					});
+					descDetail.css({
+						opacity:'0'
+					});
+					$('div.img-desc div.train div#img4').css({
+							opacity:'1'
+					});
+					$('div.name-desc div.train div#name4').css({
+							opacity:'1'
+					});
+					$('div.desc div.train div#desc4').css({
 						opacity:'1'
-				});
-				$('div.name-desc div.train div#name9').css({
+					});
+					currentPic( $(this).index() );
+					break;
+				case 4:
+					viewDetail.css({
+						opacity:'0'
+					});
+					nameDetail.css({
+						opacity:'0'
+					});
+					descDetail.css({
+						opacity:'0'
+					});
+					$('div.img-desc div.train div#img5').css({
+							opacity:'1'
+					});
+					$('div.name-desc div.train div#name5').css({
+							opacity:'1'
+					});
+					$('div.desc div.train div#desc5').css({
 						opacity:'1'
-				});
-				$('div.desc div.train div#desc9').css({
-					opacity:'1'
-				});
-				currentPic( $(this).index() );
-				break;
-		}
+					});
+					currentPic( $(this).index() );
+					break;
+				case 5:
+					viewDetail.css({
+						opacity:'0'
+					});
+					nameDetail.css({
+						opacity:'0'
+					});
+					descDetail.css({
+						opacity:'0'
+					});
+					$('div.img-desc div.train div#img6').css({
+							opacity:'1'
+					});
+					$('div.name-desc div.train div#name6').css({
+							opacity:'1'
+					});
+					$('div.desc div.train div#desc6').css({
+						opacity:'1'
+					});
+					currentPic( $(this).index() );
+					break;
+				case 6:
+					viewDetail.css({
+						opacity:'0'
+					});
+					nameDetail.css({
+						opacity:'0'
+					});
+					descDetail.css({
+						opacity:'0'
+					});
+					$('div.img-desc div.train div#img7').css({
+							opacity:'1'
+					});
+					$('div.name-desc div.train div#name7').css({
+							opacity:'1'
+					});
+					$('div.desc div.train div#desc7').css({
+						opacity:'1'
+					});
+					currentPic( $(this).index() );
+					break;
+				case 7:
+					viewDetail.css({
+						opacity:'0'
+					});
+					nameDetail.css({
+						opacity:'0'
+					});
+					descDetail.css({
+						opacity:'0'
+					});
+					$('div.img-desc div.train div#img8').css({
+							opacity:'1'
+					});
+					$('div.name-desc div.train div#name8').css({
+							opacity:'1'
+					});
+					$('div.desc div.train div#desc8').css({
+						opacity:'1'
+					});
+					currentPic( $(this).index() );
+					break;
+				case 8:
+					viewDetail.css({
+						opacity:'0'
+					});
+					nameDetail.css({
+						opacity:'0'
+					});
+					descDetail.css({
+						opacity:'0'
+					});
+					$('div.img-desc div.train div#img9').css({
+							opacity:'1'
+					});
+					$('div.name-desc div.train div#name9').css({
+							opacity:'1'
+					});
+					$('div.desc div.train div#desc9').css({
+						opacity:'1'
+					});
+					currentPic( $(this).index() );
+					break;
+			}
 	});
-
 
 	var train = $('div.slideshow-pro div.scroll'),
 		lists = $('section.product-detail div.info-pro div.slide-pro div.slideshow-pro div.scroll > div')
@@ -394,8 +392,7 @@ listDetail.click(function(){
 	});
 
 /* Main Menu ************************/
-var liList = $('nav.mainmenu li');
-	//ulList = $('nav.mainmenu li ul',this);
+	var liList = $('nav.mainmenu li');
 
 	liList.mouseover(function(){
 		$('ul',this).animate({
