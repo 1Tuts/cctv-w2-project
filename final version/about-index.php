f0b19a2a8837e44f0950e65b70e40c9491273560
zohreh-z <zahedi.zohreh@gmail.com>
2012-10-10 15:26:21 +0330

    seprate files.... finish

--- final version/about-index.php
+++ final version/about-index.php
@@ -1,47 +1,5 @@
-		<?php
-			include('inc/header.php');
-		?>
-
-		<!-- start About info -->
-		<section class="about-content cover">
-
-			<div class="nav cover mt2">
-				<a href="#">درباره شرکت</a>
-				<span></span>
-				<a class="next-link" href="#">درباره ما</a>
-
-			</div>
-			<div class="top-pic"></div>
-			<div class="about-info cover">
-				<?php
-					include('inc/left-menu.php');
-				?>
-
-				<div class="info left">
-					<p class="mt1">
-						از زمان تاسیس آن در سال 1976، ایسر، هدف خود را از شکستن موانع بین مردم و فن آوری به دست آورد. در سطح جهانی، ایسر در رتبه شماره 2 برای نوت بوک PCs.1 کانال مدل کسب و کار سود آور و پایدار به رشد این شرکت ادامه ابزاری است، در حالی که رویکرد خود را چند نام تجاری ادغام ایسر، دروازه، پاکارد بل، و مارک های آغازین در بازارهای جهانی است.
-					</p>
-					<img src="images/about/about_us_01.jpg"/>
-					<img src="images/about/about_us_02.jpg"/>
-					<p class="mt1 mb3">
-						ایسر طراحی محصولات سازگار با محیط زیست است و یک زنجیره تامین سبز را از طریق همکاری با تامین کنندگان تاسیس شده است. ایسر مفتخر است که به عنوان یک شریک در سراسر جهان از جنبش المپیک، از جمله حمایت از ونکوور 2010 المپیک زمستانی بازی ها و بازی های المپیک 2012 لندن است.
-					<br />
-					<br />
-						گروه ایسر از 8،000 نفر را در سراسر جهان. درآمد در سال 2011 به 15،7 میلیارد دلار آمریکا رسیده است.<br />
-						<a href="#"> www.acer-group.com </a> : برای کسب اطلاعات بیشتر
-					<br />
-					<br />
-					<br />
-					<br />
-					<br />
-					<br />
-					<br />
-					<span><sup>1</sup>Source, Gartner data 2011.</span>
-					</p>
-				</div>
-			</div>
-			<div class="bottom-pic"></div>
-		</section>
-		<?php
-			include('inc/footer.php');
-		?>
+<?php
+	include('inc/header.php');
+	include('body/body-about.php');
+	include('inc/footer.php');
+?>
---
+++ final version/body/body-catalog.php
@@ -0,0 +1,26 @@
+<!-- start contact info -->
+		<section class="catalog cover">
+			<div class="nav cover mt2">
+				<a href="#">درباره شرکت</a>
+				<span></span>
+				<a class="next-link" href="#">کاتالوگ</a>
+			</div>
+			<div class="top-pic"></div>
+			<div class="catalog-info cover">
+				<div class="left-txt">
+					<p>در این بخش شما میتوانید مقاله مورد نظر خود را دانلود کنید.</p>
+				</div>
+				<div class="info left">
+					<ul>
+							<li><a href="download/4.zip"><span class="download">صادرات این گروه</span><span class="list">نت بوک</span></a></li>
+							<li><a href="download/101.zip"><span class="download">صادرات این گروه</span><span class="list">دسک تاب</span></a></li>
+							<li><a href="#"><span class="download">صادرات این گروه</span><span class="list">مانیتور</span></a></li>
+							<li><a href="#"><span class="download">صادرات این گروه</span><span class="list">پروژکتور</span></a></li>
+							<li><a href="#"><span class="download">صادرات این گروه</span><span class="list">تبلت</span></a></li>
+							<li><a href="#"><span class="download">صادرات این گروه</span><span class="list">انتخابی</span></a></li>
+							<li><a href="#"><span class="download">صادرات این گروه</span><span class="list">Ultrabooks</span></a></li>
+						</ul>	
+				</div>
+			</div>
+			<div class="bottom-pic"></div>
+		</section>
